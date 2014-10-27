cb7f7636ec3c8bf1b5785305d3a0e0dacd5c85d4
Zhibo Qi <qzb0015@gmail.com>
2014-10-27 14:00:39 +0800

    Change the BlockController

--- WPF2048/Block.cs
+++ WPF2048/Block.cs
@@ -7,7 +7,10 @@ namespace WPF2048
 {
     class Block
     {
-        public int num { set; get; }
-        public int moved { set; get; } //move step count
+        public int num;
+        public int movesteps { set; get; }  //move step count
+        public int status { set; get; }     //judge the block new or die 0=>old, 1=>died, 2=>new
+        public int oldRow { set; get; }     //the last Row of the block
+        public int oldColumn { set; get; }  //the last Column of the block
     }
 }
